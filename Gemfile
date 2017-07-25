f3fe9d742127c8cd35038c6ce433afa026339b61
Imogen Hardy <imogen.hardy@gmail.com>
2017-07-25 15:42:07 +0100

    setting up for Heroku deployment

--- Gemfile
+++ Gemfile
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
- gem 'capybara'
  gem 'data_mapper'
  gem 'database_cleaner'
  gem 'dm-postgres-adapter'
- gem 'rspec'
  gem 'sinatra'
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+end
--- app/models/link.rb
+++ app/models/link.rb
@@ -14,7 +14,7 @@ class Link
 end
 
 # Now let's set up a connection with a database
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 # let's check that everything we wrote in our models was OK
 DataMapper.finalize
 # And let's build any new colums or tables we added
