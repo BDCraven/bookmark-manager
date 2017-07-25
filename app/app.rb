e52d1bf3aec1e1ecdbe7a3937ed3485196eb79c6
BDCraven <bencraven@hotmail.com>
2017-07-25 16:49:05 +0100

    User story: allowing links to have tags associated with them

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,8 @@
-require 'sinatra/base'
-require_relative 'models/link'
 ENV["RACK_ENV"] ||= "development"
+
+require 'sinatra/base'
+# require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
@@ -17,7 +19,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url], title: params[:title])  # 1. Create a link
+    tag = Tag.first_or_create(name: params[:tags]) # 2. Create a tag for the link
+    link.tags << tag # 3. Adding the tag to the link's DataMapper collection.
+    link.save # 4. Saving the link.
     redirect '/links'
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,8 @@ class Link
   # add DataMapper functionality to this class so it can communicate with the database
   include DataMapper::Resource
 
+  has n, :tags, through: Resource
+
   # these property declarations set the column headers in the 'links' table
   property :id,    Serial #Serial means that it will be auto-incremented for every record
   property :title, String
@@ -13,9 +15,9 @@ class Link
 
 end
 
-# Now let's set up a connection with a database
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-# let's check that everything we wrote in our models was OK
-DataMapper.finalize
-# And let's build any new colums or tables we added
-DataMapper.auto_upgrade!
+# # Now let's set up a connection with a database
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+# # let's check that everything we wrote in our models was OK
+# DataMapper.finalize
+# # And let's build any new colums or tables we added
+# DataMapper.auto_upgrade!
