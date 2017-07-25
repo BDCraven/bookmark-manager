2ba2c1ab775859de97ac74d41a6662b757ef9778
BDCraven <bencraven@hotmail.com>
2017-07-25 12:41:37 +0100

    Failing test: user story: view links on page. Construct a Link model that uses Datamapper ORM

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,9 @@
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Testing infrastructure working!'
-  end
+  # get '/' do
+  #   'Testing infrastructure working!'
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,9 @@
 ENV['RACK_ENV'] = 'test'
 
 # require our Sinatra app file
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '../app', 'app.rb')
 
+require './app/models/link'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
